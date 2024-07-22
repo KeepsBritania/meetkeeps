ff6fa8b6199ca4321aebe7dbbb5aff0ac871fd37
Iamkeeper <36058486+KeepsBritania@users.noreply.github.com>
2024-07-22 09:54:03 +0800

    Add files via upload

--- assets/js/steller.js
+++ assets/js/steller.js
@@ -27,3 +27,39 @@ document.addEventListener('DOMContentLoaded', function() {
 		}, 100);
 	});
 });
+
+
+// Get all the modals
+var modals = document.querySelectorAll(".modal");
+
+// Get all the buttons that open the modals
+var btns = document.querySelectorAll(".open-modal");
+
+// Get all the <span> elements that close the modals
+var spans = document.querySelectorAll(".close");
+
+// When the user clicks the button, open the corresponding modal 
+btns.forEach(function(btn) {
+    btn.onclick = function() {
+        var modal = document.getElementById(this.dataset.modal);
+        modal.style.display = "block";
+    }
+});
+
+// When the user clicks on <span> (x), close the modal
+spans.forEach(function(span) {
+    span.onclick = function() {
+        modals.forEach(function(modal) {
+            modal.style.display = "none";
+        });
+    }
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    modals.forEach(function(modal) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+}
